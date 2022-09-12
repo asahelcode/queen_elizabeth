1fa73d25479b09c7f255fe1698966638fb83feaf
Rita-Bomki <ritabomki@gmail.com>
2022-09-12 14:57:37 +0000

    create react app

---
+++ Desktop/Build/queen_elizabeth/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
